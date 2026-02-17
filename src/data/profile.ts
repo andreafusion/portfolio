260ff79e126c23267148424bae12dc01744acf8d
andreafusion <andreafusiondigital@gmail.com>
2026-02-17 15:02:29 +0100

    skills ts created and profilets info fixed

--- src/data/profile.ts
+++ src/data/profile.ts
@@ -1,5 +1,5 @@
 export const PROFILE = {
-  name: "TU_NOMBRE",
+  name: "Andrea",
   shortName: "Andrea", // lo que sale grande tipo “Rafa.”
   greeting: "Hey, I'm",
   headlinePrefix: "And I",
@@ -11,5 +11,38 @@ export const PROFILE = {
   hobbies: ["Boxing", "Anime", "Music"],
   ctaTitle: "Let's build something together",
   ctaSubtitle: "Contact me",
-  altText: "TU_NOMBRE",
+  altText: "Andrea Morant",
+  contact: {
+    email: "andreafusiondigital@gmail.com",
+    location: "Valencia, Spain",
+    availabilityBadge: "Available for work", // o lo que quieras
+    contactTitle: "Let's keep in touch.",
+    contactSubtitle:
+      "Got a project in mind? Let's build something amazing together.",
+  },
+
+  socialLinks: [
+    {
+      name: "GitHub",
+      url: "https://github.com/andreafusion",
+      icon: "github",
+      color: "hover:bg-gray-900 hover:text-white",
+      label: "Check my code",
+    },
+    {
+      name: "Instagram",
+      url: "https://www.instagram.com/andrea.morant/",
+      icon: "github",
+      color: "hover:bg-black hover:text-white",
+      label: "Follow me",
+    },
+    {
+      name: "LinkedIn",
+      url: "https://www.linkedin.com/in/andrea-guijarro-morant/",
+      icon: "github",
+      color: "hover:bg-[#0077b5] hover:text-white",
+      label: "Let's connect",
+    },
+    // añade LinkedIn / X / Instagram si quieres
+  ],
 };
